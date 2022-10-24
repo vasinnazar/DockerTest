7c2b982302965e432436dee7b360e88c8b50069c
AlexSvT <alexreih.95@gmail.com>
2022-10-24 09:26:08 +0700

    Убрал подключение к локальной бд из CourtOrder.php

--- app/CourtOrder.php
+++ app/CourtOrder.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class CourtOrder extends Model
 {
     const TEXT_ID_FORM_ORDER = 'court_order';
-    protected $connection='debtors1';
     protected $fillable=[
       'debtor_id',
       'is_printed',
