c048c39352028644cd8ad3c14bfacd2d5ab62f5b
AlexSvT <a.golobokov@fterra.ru>
2023-02-03 15:28:25 +0700

    delete magic numbers

--- app/DebtorsEventType.php
+++ app/DebtorsEventType.php
@@ -8,6 +8,9 @@ class DebtorsEventType extends Model
 {
     protected $table = 'debtors.debtors_event_types';
     protected $fillable = ['name'];
+
+    const TYPE_INFORMATION = 9;
+    const RESULT_TYPE_CONSENT_TO_PEACE = 19;
     
     /**
      * Возвращает массив всех типов мероприятий по должникам
