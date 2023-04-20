19ac6f43c26d9f3e78138ab3ff2b2a7819a626de
AlexSvT <a.golobokov@fterra.ru>
2023-04-20 14:34:40 +0700

    убрал тип

--- app/Repositories/DebtorEventSmsRepository.php
+++ app/Repositories/DebtorEventSmsRepository.php
@@ -18,7 +18,7 @@ class DebtorEventSmsRepository
         return $this->model->findOrFail($id);
     }
 
-    public function findByCustomerAndSmsId(string $customerId1C, int $smsId): DebtorEventSms
+    public function findByCustomerAndSmsId(string $customerId1C, int $smsId)
     {
         return $this->model->where('customer_id_1c', $customerId1C)
             ->where('sms_id', $smsId)
