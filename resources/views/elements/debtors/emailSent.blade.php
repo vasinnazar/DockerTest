7fe2a29f3eddf069407cf689b72d19998031db29
Irina <kemirinaou@gmail.com>
2023-06-22 15:17:12 +0700

    added form of sending emails

---
+++ resources/views/elements/debtors/emailSent.blade.php
@@ -0,0 +1,20 @@
+<div class="modal fade" id="debtorEmailSent" tabindex="-1" role="dialog" aria-labelledby="debtorEmailSentLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                 <h4 class="modal-title">Отправленные Email</h4>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-xs-12">
+
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
+            </div>
+        </div>
+    </div>
+</div>
