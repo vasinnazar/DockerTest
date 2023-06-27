814ceeb8a0a6b7b4f9d9575483ea7e7b9a75db87
Irina <kemirinaou@gmail.com>
2023-06-27 15:43:03 +0700

    fixed RolesSeeder, added tests mass sending email

--- tests/Feature/DebtorMassSmsControllerTest.php
+++ tests/Feature/DebtorMassSmsControllerTest.php
@@ -11,6 +11,7 @@ use App\EmailMessage;
 use App\Loan;
 use App\LoanType;
 use App\Passport;
+use App\Repositories\DebtorEventsRepository;
 use App\Services\MessageService;
 use App\Subdivision;
 use App\User;
@@ -27,56 +28,66 @@ class DebtorMassSmsControllerTest extends TestCase
 {
     use RefreshDatabase;
 
+    private $user;
+    private $debtors;
+    private $emailTemplateId;
+    private $eventTypeId = 24;
+    private DebtorEventsRepository $debtorEventsRepository;
+    public function __construct(?string $name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->debtorEventsRepository = app()->make(DebtorEventsRepository::class);
+    }
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = factory(User::class)->create();
+        factory(LoanType::class)->create();
+        factory(Subdivision::class)->create();
+        $this->debtors = factory(Debtor::class, 'debtor', 5)->create();
+        foreach ($this->debtors as $debtor) {
+            $customer = factory(Customer::class)->create([
+                'id_1c' => $debtor->customer_id_1c
+            ]);
+            $passport = factory(Passport::class)->create([
+                'customer_id' => $customer->id
+            ]);
+            $claim = factory(Claim::class)->create([
+                'customer_id' => $customer->id,
+                'passport_id' => $passport->id
+            ]);
+            factory(Loan::class)->create([
+                'id_1c' => $debtor->loan_id_1c,
+                'claim_id' => $claim->id
+            ]);
+        }
+
+        $this->seed(RolesSeeder::class);
+        $this->seed(EmailsMessagesSeeder::class);
+        $this->emailTemplateId = EmailMessage::all()->pluck('id')->toArray();
+
+    }
     public function testSendMassMessageSms()
     {
         $isSms = 1;
-        $debtors = factory(Debtor::class, 'debtor', 5)->create();
-        $user = factory(User::class)->create();
         $sms = factory(DebtorSmsTpls::class, 'sms')->create();
-        $customers = collect();
-        foreach ($debtors as $debtor) {
-            $customers->push(factory(Customer::class)->create([
-                'id_1c' => $debtor->customer_id_1c
-            ]));
-        }
-
         $this->withoutMiddleware();
-        $response = $this->actingAs($user, 'web')
+        $response = $this->actingAs($this->user, 'web')
             ->post('/ajax/debtors/massmessage/send', [
                 'isSms' => $isSms,
-                'responsibleUserId' => $user->id,
-                'debtorsIds' => $debtors->pluck('id')->toArray(),
+                'responsibleUserId' => $this->user->id,
+                'debtorsIds' => $this->debtors->pluck('id')->toArray(),
                 'templateId' => $sms->id,
                 'sendDate' => Carbon::now()->format('d.m.Y'),
             ]);
 
         $result = $response->decodeResponseJson();
-        $this->assertEquals($debtors->count(), (int)$result['cnt']);
+        $this->assertEquals($this->debtors->count(), (int)$result['cnt']);
         $this->assertEquals("success", $result['error']);
     }
-
-    public function testSendMassMessageEmail()
+    public function testSendMassMessageEmailSuccessSending()
     {
         $isSms = 0;
-        $user = factory(User::class)->create();
-        $debtors = factory(Debtor::class, 'debtor', 1)->create();
-        $customers = collect();
-        foreach ($debtors as $debtor) {
-            $customers->push(factory(Customer::class)->create([
-                'id_1c' => $debtor->customer_id_1c
-            ]));
-        }
-        factory(Passport::class)->create();
-        factory(Claim::class)->create();
-        factory(LoanType::class)->create();
-        factory(Subdivision::class)->create();
-        factory(Loan::class)->create([
-            'id_1c' => $debtors->pluck('loan_id_1c')->first()
-        ]);
-        $this->seed(RolesSeeder::class);
-        $this->seed(EmailsMessagesSeeder::class);
-        $emailTemplateId = EmailMessage::all()->pluck('id')->toArray();
-
         $this->app->bind(
             ArmClient::class,
             function () {
@@ -85,19 +96,10 @@ class DebtorMassSmsControllerTest extends TestCase
                     (object) ['id' => 1]
                 ]));
                 $mock->shouldReceive('getAbouts')->andReturn([
-                    ['id' => 1,  'email' => "test@mail.ru"]
+                    ['id' => 1,  'email' => "test_customer@mail.ru"]
                 ]);
                 $mock->shouldReceive('getUserById1c')->andReturn([
-                    [
-                        'id' => 1,
-                        "email_user" =>  [
-                            "id" => 1,
-                            "email" => "test@mail.ru",
-                            "password" => "123456",
-                            "user_id" => 1
-                        ]
-                    ]
-                ]);
+                    ["email_user" =>  ["email" => "test_user@mail.ru", "password" => "123456"]]]);
                 return $mock;
             }
         );
@@ -106,29 +108,126 @@ class DebtorMassSmsControllerTest extends TestCase
             MessageService::class,
             function () {
                 $mockSendEmail = Mockery::mock(MessageService::class);
-                $mockSendEmail->shouldReceive('sendEmailMessage')->once()->andReturn(true);
+                $mockSendEmail->shouldReceive('sendEmailMessage')->andReturn(true);
                 return $mockSendEmail;
             }
         );
 
         $this->withoutMiddleware();
-        $response = $this->actingAs($user, 'web')
+        $response = $this->actingAs($this->user, 'web')
             ->post('/ajax/debtors/massmessage/send', [
                 'isSms' => $isSms,
-                'responsibleUserId' => $user->id,
-                'debtorsIds' => $debtors->pluck('id')->toArray(),
-                'templateId' => Arr::random($emailTemplateId),
+                'responsibleUserId' => $this->user->id,
+                'debtorsIds' => $this->debtors->pluck('id')->toArray(),
+                'templateId' => Arr::random($this->emailTemplateId),
                 'sendDate' => Carbon::now()->format('d.m.Y'),
             ]);
         $result = $response->decodeResponseJson();
-        $this->assertEquals($debtors->count(), (int)$result['cnt']);
-        $this->assertDatabaseHas('debtor_event_email', [
-            'debtor_id' => $debtors->pluck('id')[0],
-            'status' => (int) true
+
+        foreach ($this->debtors as $debtor) {
+            $this->assertDatabaseHas('debtor_event_email', [
+                'debtor_id' => $debtor->id,
+                'status' => (int) true
             ]);
-        $this->assertDatabaseHas('debtor_events', [
-            'debtor_id' => $debtors->pluck('id')[0],
-            'event_type_id' => 24
-        ]);
+            $this->assertDatabaseHas('debtor_events', [
+                'debtor_id' => $debtor->id,
+                'event_type_id' => $this->eventTypeId
+            ]);
+        }
+        $this->assertEquals($this->debtors->count(), (int)$result['cnt']);
+    }
+    public function testSendMassMessageEmailErrorSending()
+    {
+        $isSms = 0;
+        $this->app->bind(
+            ArmClient::class,
+            function () {
+                $mock = Mockery::mock(ArmClient::class);
+                $mock->shouldReceive('getCustomerById1c')->andReturn(new Collection([
+                    (object) ['id' => 1]
+                ]));
+                $mock->shouldReceive('getAbouts')->andReturn([
+                    ['id' => 1,  'email' => "test_customer@mail.ru"]
+                ]);
+                $mock->shouldReceive('getUserById1c')->andReturn([
+                    ["email_user" =>  ["email" => "test_user@mail.ru", "password" => "123456"]]]);
+                return $mock;
+            }
+        );
+
+        $this->app->bind(
+            MessageService::class,
+            function () {
+                $mockSendEmail = Mockery::mock(MessageService::class);
+                $mockSendEmail->shouldReceive('sendEmailMessage')->andReturn(false);
+                return $mockSendEmail;
+            }
+        );
+        $response = $this->actingAs($this->user, 'web')
+            ->post('/ajax/debtors/massmessage/send', [
+                'isSms' => $isSms,
+                'responsibleUserId' => $this->user->id,
+                'debtorsIds' => $this->debtors->pluck('id')->toArray(),
+                'templateId' => Arr::random($this->emailTemplateId),
+                'sendDate' => Carbon::now()->format('d.m.Y'),
+            ]);
+        $result = $response->decodeResponseJson();
+
+        foreach ($this->debtors as $debtor) {
+            $this->assertDatabaseHas('debtor_event_email', [
+                'debtor_id' => $debtor->id,
+                'status' => (int) false
+            ]);
+            $this->assertDatabaseMissing('debtor_events', [
+                'debtor_id' => $debtor->id,
+                'event_type_id' => $this->eventTypeId
+            ]);
+        }
+        $this->assertEquals(0, (int)$result['cnt']);
+    }
+    public function testSendMassMessageEmailErrorGetEmailFromArm()
+    {
+        $isSms = 0;
+        $this->app->bind(
+            ArmClient::class,
+            function () {
+                $mock = Mockery::mock(ArmClient::class);
+                $mock->shouldReceive('getCustomerById1c')->andReturn(new Collection([
+                    (object) ['id' => 1]
+                ]));
+                $mock->shouldReceive('getAbouts')->andReturn([
+                    ['id' => 1,  'email' => "test_customer@mail.ru"]
+                ]);
+                $mock->shouldReceive('getUserById1c')->andReturn([
+                    ["email_user" =>  []]]);
+                return $mock;
+            }
+        );
+        $response = $this->actingAs($this->user, 'web')
+            ->post('/ajax/debtors/massmessage/send', [
+                'isSms' => $isSms,
+                'responsibleUserId' => $this->user->id,
+                'debtorsIds' => $this->debtors->pluck('id')->toArray(),
+                'templateId' => Arr::random($this->emailTemplateId),
+                'sendDate' => Carbon::now()->format('d.m.Y'),
+            ]);
+        $result = $response->decodeResponseJson();
+        $this->assertEquals(0, (int)$result['cnt']);
+    }
+    public function testSendMassMessageEmailErrorExcLimit()
+    {
+        $isSms = 0;
+        $this->debtorEventsRepository->createEvent($this->debtors->first(),$this->user,'test', $this->eventTypeId, 0);
+        $this->debtorEventsRepository->createEvent($this->debtors->first(),$this->user,'test', $this->eventTypeId, 0);
+        $response = $this->actingAs($this->user, 'web')
+            ->post('/ajax/debtors/massmessage/send', [
+                'isSms' => $isSms,
+                'responsibleUserId' => $this->user->id,
+                'debtorsIds' => [$this->debtors->first()->id],
+                'templateId' => Arr::random($this->emailTemplateId),
+                'sendDate' => Carbon::now()->format('d.m.Y'),
+            ]);
+        $result = $response->decodeResponseJson();
+        $this->assertEquals(0, (int)$result['cnt']);
     }
 }
