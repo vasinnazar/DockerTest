8e420010a2ec16fe7d24bd8de5e70a1cdf2557f5
Irina <kemirinaou@gmail.com>
2023-08-14 15:52:37 +0700

    fixed getting fio

--- app/Http/Controllers/DebtorsController.php
+++ app/Http/Controllers/DebtorsController.php
@@ -2553,7 +2553,7 @@ class DebtorsController extends BasicController
                 return (User::where('id_1c', $debtor->responsible_user_id_1c)->first())->name;
             })
             ->addColumn('passports_fio', function ($debtor) {
-                return $debtor->passport->fio;
+                return $debtor->passport->fio ?? '';
             })
             ->rawColumns(['links'])
             ->toJson();
