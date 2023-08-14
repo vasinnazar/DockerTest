678b33fe5917ef72f6992573a1a9315a63bfe066
Irina <kemirinaou@gmail.com>
2023-08-14 15:47:55 +0700

    added filling fields of address

--- public/js/debtocard/suggest-address.js
+++ public/js/debtocard/suggest-address.js
@@ -1,18 +1,45 @@
 $(document).ready(function () {
-
+    let addressesFias;
+    $("body").click(function(e) {
+        if($(e.target).attr('id') === 'checkAddressReg') {
+            $('#address_reg').show();
+        }
+        else {
+            $('#address_reg').hide();
+        }
+    });
     function createOptionsComponent (suggestions = []) {
         let html = ``;
+        let i = 0;
+        addressesFias = [];
         for (let suggest of suggestions) {
-            html += `<li class="list-group-item" value="${suggest.value}">${suggest.value}</li>`;
+            html += `<li class="list-group-item" code="${i}" value="${suggest.value}">${suggest.value}</li>`;
+            addressesFias.push(suggest.data);
+            i++;
         }
         return html;
+    }
+    function fillAddressFields(suggestion) {
+        document.getElementById('zip').innerText = suggestion.postal_code;
+        document.getElementById('address_region').innerText = suggestion.region_with_type;
+        document.getElementById('address_district').innerText = suggestion.area_with_type;
+        document.getElementById('address_city').innerText = suggestion.city_with_type;
+        document.getElementById('address_city1').innerText = suggestion.settlement_with_type;
+        document.getElementById('address_street').innerText = suggestion.street_with_type;
+        document.getElementById('address_house').innerText = suggestion.house;
+        document.getElementById('address_building').innerText = suggestion.block;
+        document.getElementById('address_apartment').innerText = suggestion.flat;
+        document.getElementById('okato').innerText = suggestion.okato;
+        document.getElementById('oktmo').innerText = suggestion.oktmo;
+        document.getElementById('fias_code').innerText = suggestion.fias_code;
+        document.getElementById('fias_id').innerText = suggestion.fias_id;
+        document.getElementById('kladr_id').innerText = suggestion.kladr_id;
     }
     $(document).on('click', '#checkAddressReg', function() {
         $.post($.app.url + '/debtors/suggests', {
             address: $('textarea[name="address_reg"]').val()
         })
             .done(function(response) {
-                console.log(response)
                 $("#address_reg").html(createOptionsComponent(response));
                 $('#address_reg').show();
         });
@@ -29,9 +56,18 @@ $(document).ready(function () {
     $(document).on('click','#address_reg li', function(){
         $('#input_address_reg').val($(this).attr('value'));
         $('#address_reg').hide(150);
+        let addressFias = addressesFias[$(this).attr('code')];
+        console.log(addressFias);
+        fillAddressFields(addressFias);
     });
     $(document).on('click','#address_fact li', function(){
         $('#input_address_fact').val($(this).attr('value'));
         $('#address_fact').hide(150);
     });
+
+    const categoryViewForm = () => {
+        console.log(46);
+    }
+
+    window.categoryViewForm = categoryViewForm;
 });
--- resources/views/debtocard/timezone-address/modal-address-changes.blade.php
+++ resources/views/debtocard/timezone-address/modal-address-changes.blade.php
@@ -7,10 +7,10 @@
             </div>
             <div class="modal-body" style="overflow-y: auto;">
                 <div class="row">
-                    <table class='table table-bordered'>
+                    <table class='table without-border table-address-info'>
                         <tbody>
                         <tr>
-                            <td colspan="2" class="pt-1">
+                            <td colspan="4" class="pt-1">
                                 <div class="form-group row">
                                     <label for="address_reg" class="col-sm-4 col-form-label">Адрес регистрации</label>
                                     <div class="col-sm-8 pos-relative">
@@ -29,40 +29,53 @@
                         </tr>
                         <tr>
                             <th scope="row">Индекс</th>
-                            <td>{{$debtor->passport->zip}}</td>
-
+                            <td id="zip">{{$debtor->passport->zip}}</td>
+                            <th scope="row">Код ОКАТО</th>
+                            <td id="okato"></td>
                         </tr>
                         <tr>
                             <th scope="row">Регион</th>
-                            <td>{{$debtor->passport->address_region}}</td>
+                            <td id="address_region">{{$debtor->passport->address_region}}</td>
+                            <th scope="row">Код ОКТМО</th>
+                            <td id="oktmo"></td>
                         </tr>
                         <tr>
                             <th scope="row">Район</th>
-                            <td></td>
+                            <td id="address_district">{{$debtor->passport->address_district}}</td>
+                            <th scope="row">Код ФИАС</th>
+                            <td id="fias_code"></td>
                         </tr>
                         <tr>
                             <th scope="row">Город</th>
-                            <td></td>
+                            <td id="address_city">{{$debtor->passport->address_city}}</td>
+                            <th scope="row">ФИАС ИД</th>
+                            <td id="fias_id"></td>
                         </tr>
                         <tr>
                             <th scope="row">Нас. пункт</th>
-                            <td></td>
+                            <td id="address_city1">{{$debtor->passport->address_city1}}</td>
+                            <th scope="row">КЛАДР</th>
+                            <td id="kladr_id"></td>
                         </tr>
                         <tr>
                             <th scope="row">Улица</th>
-                            <td></td>
+                            <td id="address_street">{{$debtor->passport->address_street}}</td>
                         </tr>
                         <tr>
                             <th scope="row">Дом</th>
-                            <td></td>
+                            <td id="address_house">{{$debtor->passport->address_house}}</td>
                         </tr>
                         <tr>
                             <th scope="row">Корпус</th>
-                            <td></td>
+                            <td id="address_building">{{$debtor->passport->address_building}}</td>
                         </tr>
                         <tr>
-                            <td colspan="2" class="pt-1">
-                                <br>
+                            <th scope="row">Квартира</th>
+                            <td id="address_apartment">{{$debtor->passport->address_apartment}}</td>
+                        </tr>
+                        <tr>
+                            <td colspan="4" class="pt-1">
+                                <hr>
                                 <div class="form-group row">
                                     <label for="address_fact" class="col-sm-4 col-form-label">Адрес проживания</label>
                                     <div class="col-sm-8 pos-relative">
@@ -81,35 +94,48 @@
                         <tr>
                             <th scope="row">Индекс</th>
                             <td>{{$debtor->passport->fact_zip}}</td>
-
+                            <th scope="row">Код ОКАТО</th>
+                            <td>{{$debtor->passport->fact_okato}}</td>
                         </tr>
                         <tr>
                             <th scope="row">Регион</th>
-                            <td></td>
+                            <td>{{$debtor->passport->fact_address_region}}</td>
+                            <th scope="row">Код ОКТМО</th>
+                            <td>{{$debtor->passport->fact_oktmo}}</td>
                         </tr>
                         <tr>
                             <th scope="row">Район</th>
-                            <td></td>
+                            <td>{{$debtor->passport->fact_address_district}}</td>
+                            <th scope="row">Код ФИАС</th>
+                            <td>{{$debtor->passport->fact_fias_id}}</td>
                         </tr>
                         <tr>
                             <th scope="row">Город</th>
-                            <td></td>
+                            <td>{{$debtor->passport->fact_address_city}}</td>
+                            <th scope="row">ФИАС ИД</th>
+                            <td>{{$debtor->passport->fact_fias_id}}</td>
                         </tr>
                         <tr>
                             <th scope="row">Нас. пункт</th>
-                            <td></td>
+                            <td>{{$debtor->passport->fact_address_city1}}</td>
+                            <th scope="row">КЛАДР</th>
+                            <td>{{$debtor->passport->fact_kladr_id}}</td>
                         </tr>
                         <tr>
                             <th scope="row">Улица</th>
-                            <td></td>
+                            <td>{{$debtor->passport->fact_address_street}}</td>
                         </tr>
                         <tr>
                             <th scope="row">Дом</th>
-                            <td></td>
+                            <td>{{$debtor->passport->fact_address_house}}</td>
                         </tr>
                         <tr>
                             <th scope="row">Корпус</th>
-                            <td></td>
+                            <td>{{$debtor->passport->fact_address_building}}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Квартира</th>
+                            <td>{{$debtor->passport->fact_address_apartment}}</td>
                         </tr>
                         </tbody>
                     </table>
@@ -117,7 +143,7 @@
             </div>
 
             <div class="modal-footer">
-                <button type="button" class="btn btn-primary">Сохранить</button>
+                <button type="button" class="btn btn-primary" onclick="categoryViewForm()">Сохранить</button>
                 <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
             </div>
         </div>
