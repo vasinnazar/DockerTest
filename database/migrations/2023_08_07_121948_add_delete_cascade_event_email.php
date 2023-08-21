f5a6c7437aa5d6aa4d42480ef705d8e9c0125ebf
Irina <kemirinaou@gmail.com>
2023-08-21 14:13:44 +0700

    4459_deleting_events: added delete cascade event sms

--- database/migrations/2023_08_07_121948_add_delete_cascade_event_email.php
+++ database/migrations/2023_08_07_121948_add_delete_cascade_event_email.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddDeleteCascadeEvent extends Migration
+class AddDeleteCascadeEventEmail extends Migration
 {
     /**
      * Run the migrations.
