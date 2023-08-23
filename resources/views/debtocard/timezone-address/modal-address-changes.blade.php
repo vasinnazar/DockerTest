c2028be0fea08039b8b0e2961f181696945bca03
Irina <kemirinaou@gmail.com>
2023-08-23 17:46:43 +0700

    4507_change_address_debtors: fixed suggest address

--- resources/views/debtocard/timezone-address/modal-address-changes.blade.php
+++ resources/views/debtocard/timezone-address/modal-address-changes.blade.php
@@ -5,41 +5,55 @@
                 <b>Изменение адресов</b>
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
             </div>
-            <div class="modal-body" style="overflow-y: auto;">
-                <div class="row">
-                    <div class="input-address">
-                        <div class="form-group row">
-                            <label for="address_reg" class="col-sm-4 col-form-label">Адрес регистрации</label>
-                            <div class="col-sm-8 pos-relative">
+            <div class="input-address">
+                <div class="form-group row">
+                    <label for="address_reg" class="col-sm-4 col-form-label">Адрес регистрации</label>
+                    <div class="col-sm-8 pos-relative">
                                         <textarea id="address_reg" name="address_reg" type="text"
                                                   class="form-control" value=""
                                                   autocomplete="off">{{$debtor->passport->full_address}}</textarea>
-                                <ul class="list-group ul-address" id="list_address_reg">
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="text-right mtb-10">
-                            <button class="btn btn-outline-secondary" id="checkAddressReg">
-                                Проверить
-                            </button>
-                        </div>
-                        <div class="form-group row">
-                            <label for="address_fact" class="col-sm-4 col-form-label">Адрес проживания</label>
-                            <div class="col-sm-8 pos-relative">
+                        <ul class="list-group ul-address" id="list_address_reg">
+                        </ul>
+                    </div>
+                </div>
+                <div class="text-right mtb-10">
+                    <button
+                            type="button"
+                            class="btn btn-outline-secondary"
+                            id="checkAddressReg"
+                            onclick="showSuggestions('address_reg', 'list_address_reg', false)"
+                    >
+                        Проверить
+                    </button>
+                </div>
+                <div class="form-group row">
+                    <label for="address_fact" class="col-sm-4 col-form-label">Адрес проживания</label>
+                    <div class="col-sm-8 pos-relative">
                                         <textarea id="address_fact" name="address_fact" type="text"
                                                   class="form-control" value=""
                                                   autocomplete="off">{{$debtor->passport->fact_full_address}}</textarea>
-                                <ul class="list-group ul-address" id="list_address_fact">
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="text-right mtb-10">
-                            <button class="btn btn-outline-secondary" id="checkAddressFact">
-                                Проверить
-                            </button>
-                        </div>
+                        <ul class="list-group ul-address" id="list_address_fact">
+                        </ul>
                     </div>
-                    <form id="addressCustomer">
+                </div>
+                <div class="text-right mtb-10">
+                    <button
+                            type="button"
+                            class="btn btn-outline-secondary"
+                            id="checkAddressFact"
+                            onclick="showSuggestions('address_fact', 'list_address_fact', true)"
+                    >
+                        Проверить
+                    </button>
+                </div>
+            </div>
+            <form
+                id="addressCustomer"
+                method="POST"
+                onsubmit="updatePassport(event, '{!! csrf_token() !!}', {!! $dataArm['customerId'] ?? null !!}, {!! $dataArm['passportId'] ?? null !!})"
+            >
+                <div class="modal-body" style="overflow-y: auto;">
+                    <div class="row">
                         <div class="col-sm-6">
                             <p>Полный адрес регистрации</p>
                             <label>Индекс</label>
@@ -94,7 +108,8 @@
                             <input type="text" class="form-control" id="fact_address_region" name="fact_address_region"
                                    value="{{$debtor->passport->fact_address_region}}" disabled>
                             <label>Район</label>
-                            <input type="text" class="form-control" id="fact_address_district" name="fact_address_district"
+                            <input type="text" class="form-control" id="fact_address_district"
+                                   name="fact_address_district"
                                    value="{{$debtor->passport->fact_address_district}}" disabled>
                             <label>Город</label>
                             <input type="text" class="form-control" id="fact_address_city" name="fact_address_city"
@@ -109,10 +124,12 @@
                             <input type="text" class="form-control" id="fact_address_house" name="fact_address_house"
                                    value="{{$debtor->passport->fact_address_house}}" disabled>
                             <label>Корпус</label>
-                            <input type="text" class="form-control" id="fact_address_building" name="fact_address_building"
+                            <input type="text" class="form-control" id="fact_address_building"
+                                   name="fact_address_building"
                                    value="{{$debtor->passport->fact_address_building}}" disabled>
                             <label>Квартира</label>
-                            <input type="text" class="form-control" id="fact_address_apartment" name="fact_address_apartment"
+                            <input type="text" class="form-control" id="fact_address_apartment"
+                                   name="fact_address_apartment"
                                    value="{{$debtor->passport->fact_address_apartment}}" disabled>
                             <label>Код ОКАТО</label>
                             <input type="text" class="form-control" id="fact_okato" name="fact_okato"
@@ -130,22 +147,19 @@
                             <input type="text" class="form-control" id="fact_kladr_id" name="fact_kladr_id"
                                    value="{{$debtor->passport->fact_kladr_id}}" disabled>
                         </div>
-                    </form>
+                    </div>
                 </div>
-            </div>
 
-            <div class="modal-footer">
-                <button
-                        type="button"
-                        class="btn btn-primary"
-                        onclick="updatePassport(
-                            {{$dataArm['customerId'] ?? null}},
-                            {{$dataArm['passportId']}}
-                        )">
-                    Сохранить
-                </button>
-                <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
-            </div>
+                <div class="modal-footer">
+                    <button
+                            type="submit"
+                            class="btn btn-primary"
+                    >
+                        Сохранить
+                    </button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
