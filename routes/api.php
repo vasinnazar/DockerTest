481edfd9eb1d009f67c2813b94ec2d2f34240a7b
Irina <kemirinaou@gmail.com>
2023-09-22 11:01:39 +0700

    4742_change_route_send_responsibl: web route move to api

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('/debtors')->group(function () {
 
+    Route::post('/getResponsibleUser', [FromDebtorsController::class,'respUserForSellingARM'])->middleware(['auth.basic.once']);
     Route::post('/msg/on-subdivision', [ApiDebtorsController::class,'onSubdivision']);
     Route::post('/onsite', [ApiDebtorsController::class,'onSite']);
     Route::post('/events/without-accept', [ApiEventsController::class,'withoutAcceptEvent']);
