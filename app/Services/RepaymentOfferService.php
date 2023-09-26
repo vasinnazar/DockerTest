ee81b58115199556c0e5f60b2466cc90e4686beb
Irina <kemirinaou@gmail.com>
2023-09-26 11:39:51 +0700

    4749_change_cndition_autopeace_upr: sending autopeace move to function

--- app/Services/RepaymentOfferService.php
+++ app/Services/RepaymentOfferService.php
@@ -53,50 +53,48 @@ class RepaymentOfferService
             if ($amount < 500000) {
                 continue;
             }
-
             $amount = (int)($amount * 0.3);
 
-            Log::info('Repayment Offer Auto Peace SEND:',
-                ['debtorID' => $debtor->id, 'loanId1c' => $debtor->loan_id_1c]);
-            $result = $this->armClient->sendRepaymentOffer(
-                self::REPAYMENT_TYPE_PEACE,
-                60,
-                $amount,
-                $debtor->loan_id_1c,
-                Carbon::now()->addDay(14),
-                Carbon::now(),
-                0,
-                1
-            );
+            $this->sendPeaceForUPR($debtor, $amount);
+        }
+    }
 
-            if (!$result) {
-                continue;
-            }
-
-            try {
-                $user = User::where('banned', 0)->where('id_1c', $debtor->responsible_user_id_1c)->first();
-                $event = new DebtorEvent();
-                $event->customer_id_1c = $debtor->customer_id_1c;
-                $event->event_type_id = DebtorsEventType::TYPE_INFORMATION;
-                $event->debt_group_id = $debtor->debt_group_id;
-                $event->event_result_id = DebtorsEventType::RESULT_TYPE_CONSENT_TO_PEACE;
-                $event->report = '(Автоматическое) Предварительное согласие по договору ' .
-                    $debtor->loan_id_1c . ' на мировое соглашение сроком на ' .
-                    60 . ' дней, сумма: ' .
-                    $amount / 100 . ' руб. Действует до ' .
-                    Carbon::now()->addDay(14)->format('d.m.Y');
-                $event->debtor_id = $debtor->id;
-                $event->user_id = $user->id;
-                $event->completed = 1;
-                $event->last_user_id = $user->id;
-                $event->debtor_id_1c = $debtor->debtor_id_1c;
-                $event->user_id_1c = $user->id_1c;
-                $event->refresh_date = Carbon::now()->format('Y-m-d H:i:s');
-                $event->save();
-            } catch (\Exception $e) {
-                Log::error('Create event auto-peace', ['debtorId' => $debtor->id, 'messages' => $e->getMessage()]);
-            }
-
+    public function sendPeaceForUPR(Debtor $debtor, int $amount)
+    {
+        Log::info('Repayment Offer Auto Peace SEND:',
+            ['debtorID' => $debtor->id, 'loanId1c' => $debtor->loan_id_1c]);
+        $result = $this->armClient->sendRepaymentOffer(
+            self::REPAYMENT_TYPE_PEACE,
+            60,
+            $amount,
+            $debtor->loan_id_1c,
+            Carbon::now()->addDay(14),
+            Carbon::now(),
+            0,
+            1
+        );
+        try {
+            $user = User::where('banned', 0)->where('id_1c', $debtor->responsible_user_id_1c)->first();
+            $event = new DebtorEvent();
+            $event->customer_id_1c = $debtor->customer_id_1c;
+            $event->event_type_id = DebtorsEventType::TYPE_INFORMATION;
+            $event->debt_group_id = $debtor->debt_group_id;
+            $event->event_result_id = DebtorsEventType::RESULT_TYPE_CONSENT_TO_PEACE;
+            $event->report = '(Автоматическое) Предварительное согласие по договору ' .
+                $debtor->loan_id_1c . ' на мировое соглашение сроком на ' .
+                60 . ' дней, сумма: ' .
+                $amount / 100 . ' руб. Действует до ' .
+                Carbon::now()->addDay(14)->format('d.m.Y');
+            $event->debtor_id = $debtor->id;
+            $event->user_id = $user->id;
+            $event->completed = 1;
+            $event->last_user_id = $user->id;
+            $event->debtor_id_1c = $debtor->debtor_id_1c;
+            $event->user_id_1c = $user->id_1c;
+            $event->refresh_date = Carbon::now()->format('Y-m-d H:i:s');
+            $event->save();
+        } catch (\Exception $e) {
+            Log::error('Create event auto-peace', ['debtorId' => $debtor->id, 'messages' => $e->getMessage()]);
         }
     }
 
