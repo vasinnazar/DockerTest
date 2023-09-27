0911517ee60f3854aff72bd126374218f1e5ecd7
Irina <kemirinaou@gmail.com>
2023-09-27 17:33:27 +0700

    4749_change_cndition_autopeace_upr: added repayment offer test

--- app/Services/RepaymentOfferService.php
+++ app/Services/RepaymentOfferService.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Log;
 
 class RepaymentOfferService
 {
-
     private $armClient;
     private $debtorEventsRepository;
     private $debtorRepository;
---
+++ tests/Unit/Services/RepaymentOfferServiceTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use App\Clients\ArmClient;
+use App\Debtor;
+use App\Services\RepaymentOfferService;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Mockery;
+use Tests\TestCase;
+
+class RepaymentOfferServiceTest extends TestCase
+{
+    use DatabaseTransactions;
+    private $repaymentOfferService;
+
+    public function __construct(?string $name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+    }
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->repaymentOfferService = app(RepaymentOfferService::class);
+
+        $this->debtors = factory(Debtor::class, 'debtor', 5)->create([
+            'str_podr' => '000000000006',
+            'base' => 'Б-1',
+            'debt_group_id' => 2,
+            'qty_delays' => 36
+        ]);
+
+    }
+    public function testGetEqualContactsDebtorsWithoutCustomer()
+    {
+        $this->app->bind(
+            ArmClient::class,
+            function () {
+                $mock = Mockery::mock(ArmClient::class);
+                $mock->shouldReceive('getOffers')->andReturn(['1']);
+                return $mock;
+            }
+        );
+        $this->repaymentOfferService->autoPeaceForUPR();
+    }
+
+}
