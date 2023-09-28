8279efa998ed2592fd5b42977d638f9ec90cee5f
Irina <irina@192.168.0.2>
2023-09-28 17:33:46 +0700

    added debtor block prolongation factory and test send auto peace of upr

--- tests/Unit/Services/RepaymentOfferServiceTest.php
+++ tests/Unit/Services/RepaymentOfferServiceTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Services;
 
 use App\Clients\ArmClient;
 use App\Debtor;
+use App\DebtorBlockProlongation;
 use App\Services\RepaymentOfferService;
 use App\User;
 use Carbon\Carbon;
@@ -22,10 +23,8 @@ class RepaymentOfferServiceTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
-
-
         $this->user = factory(User::class)->create();
-        $this->debtors = factory(Debtor::class, 'debtor', 5)->create([
+        $this->debtors = factory(Debtor::class, 'debtor', 10)->create([
             'responsible_user_id_1c' => $this->user->id_1c,
             'str_podr' => '000000000006',
             'base' => 'Б-1',
@@ -34,7 +33,7 @@ class RepaymentOfferServiceTest extends TestCase
         ]);
 
     }
-    public function testGetEqualContactsDebtorsWithoutCustomer()
+    public function testAutoPeaceUPRWithRepOffers()
     {
         $this->app->bind(
             ArmClient::class,
@@ -51,6 +50,78 @@ class RepaymentOfferServiceTest extends TestCase
         );
         $repaymentOfferService = app(RepaymentOfferService::class);
         $repaymentOfferService->autoPeaceForUPR();
+        foreach ($this->debtors as $debtor) {
+            $this->assertDatabaseMissing('debtor_events', [
+                'debtor_id' => $debtor->id,
+            ]);
+        }
     }
-
+    public function testAutoPeaceUPRWithDepBlockProlongation()
+    {
+        $this->app->bind(
+            ArmClient::class,
+            function () {
+                $mock = Mockery::mock(ArmClient::class);
+                $mock->shouldReceive('getOffers')->andReturn(collect());
+                $mock->shouldReceive('sendRepaymentOffer');
+                return $mock;
+            }
+        );
+        foreach ($this->debtors as $debtor) {
+            factory(DebtorBlockProlongation::class)->create([
+                'debtor_id' => $debtor->id,
+                'loan_id_1c' => $debtor->loan_id_1c,
+            ]);
+        }
+        $repaymentOfferService = app(RepaymentOfferService::class);
+        $repaymentOfferService->autoPeaceForUPR();
+        foreach ($this->debtors as $debtor) {
+            $this->assertDatabaseMissing('debtor_events', [
+                'debtor_id' => $debtor->id,
+            ]);
+        }
+    }
+    public function testAutoPeaceUPRFirstCondition()
+    {
+        $this->app->bind(
+            ArmClient::class,
+            function () {
+                $mock = Mockery::mock(ArmClient::class);
+                $mock->shouldReceive('getOffers')->andReturn(collect());
+                $mock->shouldReceive('sendRepaymentOffer');
+                return $mock;
+            }
+        );
+        $repaymentOfferService = app(RepaymentOfferService::class);
+        $repaymentOfferService->autoPeaceForUPR();
+        foreach ($this->debtors as $debtor) {
+            if ($debtor->sum_indebt >= 500000 && $debtor->sum_indebt <= 1000000) {
+                $report = '(Автоматическое) Предварительное согласие по договору ' .
+                    $debtor->loan_id_1c . ' на мировое соглашение сроком на ' .
+                    30 . ' дней, сумма: ' .
+                    (int)($debtor->sum_indebt * 0.5) / 100 . ' руб. Действует до ' .
+                    Carbon::now()->addDay(10)->format('d.m.Y');
+                $this->assertDatabaseHas('debtor_events', [
+                    'debtor_id' => $debtor->id,
+                    'report' => $report
+                ]);
+            }
+            if ($debtor->sum_indebt > 1000000) {
+                $report = '(Автоматическое) Предварительное согласие по договору ' .
+                    $debtor->loan_id_1c . ' на мировое соглашение сроком на ' .
+                    60 . ' дней, сумма: ' .
+                    (int)($debtor->sum_indebt * 0.4) / 100 . ' руб. Действует до ' .
+                    Carbon::now()->addDay(10)->format('d.m.Y');
+                $this->assertDatabaseHas('debtor_events', [
+                    'debtor_id' => $debtor->id,
+                    'report' => $report
+                ]);
+            }
+            if ($debtor->sum_indebt < 500000) {
+                $this->assertDatabaseMissing('debtor_events', [
+                    'debtor_id' => $debtor->id,
+                ]);
+            }
+        }
+    }
 }
