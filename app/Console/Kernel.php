4e22dd872d9c6286f4c930f09f3d58d1ea794856
Irina <irina@192.168.0.5>
2023-10-06 15:59:55 +0700

    4939_without_accept_job: registred command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,6 +10,7 @@ use App\Console\Commands\MysqlBackup;
 use App\Console\Commands\RepaymentOfferAutoPeace;
 use App\Console\Commands\DebtorSyncFilesImport;
 use App\Console\Commands\PassportsUpdateTimeZone;
+use App\Console\Commands\SendWithoutAccept;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Illuminate\Support\Facades\Log;
@@ -35,6 +36,7 @@ class Kernel extends ConsoleKernel
         DebtorSyncSqlProcess::class,
         PassportsUpdateTimeZone::class,
         DebtorsSearchForgotten::class,
+        SendWithoutAccept::class,
     ];
 
     /**
