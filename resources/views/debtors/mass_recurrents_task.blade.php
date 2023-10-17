834fde39abaaf80e6e86faecb6c205d3050ed949
Irina <irina@192.168.0.5>
2023-10-17 16:59:10 +0700

    4939_without_accept_job: fixed result count

--- resources/views/debtors/mass_recurrents_task.blade.php
+++ resources/views/debtors/mass_recurrents_task.blade.php
@@ -150,7 +150,7 @@
                                 </td>
                                 <td>
                                     @if ($task->completed)
-                                        Отправлено {{$task->debtors_count}}
+                                        Отправлено {{$task->debtors_processed}}
                                         <br>
                                         <span style="font-size: 80%; color: grey; font-style: italic;">{{ $task->updated_at->format('d.m.Y H:i:s') }}</span>
                                     @else
